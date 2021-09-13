87250a1f289903ad150efe91efeba6685c9c6b2e
Chahat-M <chahatmittal710@gmail.com>
2021-09-14 00:51:15 +0530

    Updated files
    Added proper spaces and comments

--- LCCM.cpp
+++ LCCM.cpp
@@ -2,115 +2,110 @@
 Least Cost Cell Method:
 Considerations: The problem is balanced i.e supply = demand
                 There is no value of supply which is same as demand (if so, then demand value is taken) - coded but not tested
-Input: Source - no. of sources - int
-       Destination - no. of destinations - int
-       Matrix input of all the constraints coeffcients - 2D matrix mat
-       Supply - supply amount of each source - int array
-       Demand - demand of each destination - int array
-Output : Optimum cost - int
-         Initial basic feasible solution - 2D matrix IBFS
-Challenge : 1. Make other entries 0 of same matrix mat instead of IBFS
-               But while doing so, final outcome turns out to be the last cell value only
-            2. reduce time complexity for iterating in the matrix vect
+Input: Source - no. of sources - int source
+       Destination - no. of destinations - int dest
+       Matrix input of all the constraints coeffcients - 2D vector vect
+       Supply - supply amount of each source - int 1D array supply
+       Demand - demand of each destination - int 1D array demand
+Output : Optimum cost - int cost
+         Initial basic feasible solution - 2D matrix ibfs
 */
 
 #include <iostream>
 #include <vector>
-using namespace std;
+#include <climits>
 
-//method to set all entries of the desired row to 0 
-vector<vector<int>> setRow(vector<vector<int>> vect,int lowi){
-  for(int i=0;i<vect[lowi].size();i++)
-     vect[lowi][i]=0;
-  return vect;
+//  Setting all entries of the desired row to 0 
+void setRow (std::vector<std::vector<int>>& vect, int lowi) {
+    for (int i=0; i < vect[lowi].size(); i++)
+        vect[lowi][i] = 0;
 }
 
-//method to set all entries of desired column to 0
-vector<vector<int>> setCol(vector<vector<int>> vect,int lowj){
-  for(int i=0;i<vect.size();i++)
-     vect[i][lowj]=0;
-  return vect;
+//  Setting all entries of desired column to 0
+void setCol (std::vector<std::vector<int>>& vect, int lowj) {
+    for (int i=0; i < vect.size(); i++)  
+        vect[i][lowj] = 0;
 }
 
-//to check if all supplies = 0 (req for break condition)
-int supply_check(int supply[],int source){
-  int sup=0;
-   for(int i=0;i<source;i++){
-     if(supply[i] != 0)
-       sup++;
+//  To check if all supplies = 0 (req for break condition)
+int supply_check (int supply[], int source) {
+    int sup = 0;
+    for (int i=0; i < source; i++) {
+        if (supply[i] != 0)
+            sup++;
     }
-  return sup;
+    return sup;
 }
 
-//to check if all demands = 0 (req for break condition)
-int demand_check(int demand[],int dest){
-  int dem=0;
-   for(int i=0;i<dest;i++){
-     if(demand[i] != 0)
-       dem++;
+//  To check if all demands = 0 (req for break condition)
+int demand_check (int demand[], int dest) {
+    int dem = 0;
+    for (int i=0; i < dest; i++) {
+        if (demand[i] != 0)
+            dem++;
     }
-  return dem;
+    return dem;
 }
 
 
 int main(){
     
-    int source,dest;
-    cout << "Enter no. of sources and destination respectively:";
-    cin>>source>>dest;
+    int source, dest;
+    std::cout << "Enter no. of sources and destination respectively:";
+    std::cin >> source >> dest;
 
-    vector<vector<int>> vect(source,vector<int> (dest));
-    vector<vector<int>> ibfs(source, vector<int> (dest,0));
+    std::vector<std::vector<int>> vect(source,std::vector<int> (dest));
+    std::vector<std::vector<int>> ibfs(source, std::vector<int> (dest,0));
   
-    //matrix input in vector form
-    cout<<"Entries of the matrix:";
-    for(int i=0;i<source;i++){
-        for(int j=0;j<dest;j++){
-            cin>>vect[i][j];
+    //  Matrix input in std::vector form
+    std::cout << "Entries of the matrix:";
+    for (int i=0; i < source; i++) {
+        for (int j=0; j < dest; j++) {
+            std::cin >> vect[i][j];
         }
     }
 
     int supply[source];
     int demand[dest];
 
-    cout<<"Enter supply quantity for each source";
-    for (int i=0;i<source;i++){
-        cin>>supply[i];
+    std::cout << "Enter supply quantity for each source";
+    for (int i=0; i < source; i++){
+        std::cin >> supply[i];
     }
 
-    cout<<"Enter demand quantity for each destination";
-    for (int i=0;i<dest;i++){
-        cin>>demand[i];
+    std::cout<<"Enter demand quantity for each destination";
+    for (int i=0; i < dest; i++) {
+        std::cin >> demand[i];
     }
 
-    //testing if supply==demand
-    int supply_sum=0;
-    int demand_sum=0;
+    // Testing if supply==demand
+    int supply_sum = 0;
+    int demand_sum = 0;
 
-    for(int i=0;i<dest;i++){
-        if(i<source){
-            supply_sum+=supply[i];
+    for (int i=0; i < dest; i++) {
+        if (i < source) {
+            supply_sum += supply[i];
         }
-        demand_sum+=demand[i];
+        demand_sum += demand[i];
     }
   
-    //needed variables
-    int low = INT8_MAX;
-    int lowi=0;
-    int lowj=0;
-    int cost=0;
+    // Needed variables
+    int low = INT_MAX;
+    int lowi = 0;
+    int lowj = 0;
+    int cost = 0;
 
-    while(supply_sum == demand_sum ){
+    while (supply_sum == demand_sum ) {
     
-        low=INT8_MAX;
+        low=INT_MAX;
 
-        if(supply_check(supply,source) == demand_check(demand,dest) && supply_check(supply,source)==0)
+        if (supply_check(supply,source) == demand_check(demand,dest) && supply_check(supply,source)==0)
             break;
 
-        // finding the least element in the matrix
-        for(int row=0;row<vect.size();row++){
-            for(int col=0;col<vect[row].size();col++){
-                if(low > vect[row][col] && vect[row][col] != 0){
+        //  Finding the least element in the matrix
+        for (int row=0; row < vect.size(); row++) {
+            for (int col=0; col < vect[row].size(); col++) {
+                if (low > vect[row][col] && vect[row][col] != 0) {
                     low = vect[row][col];
                     lowi = row;
                     lowj = col;
@@ -118,41 +113,42 @@ int main(){
             }
         }
 
-        if(supply[lowi] < demand[lowj]){
+        if (supply[lowi] < demand[lowj]) {
             cost += vect[lowi][lowj] * supply[lowi];
             ibfs[lowi][lowj] = supply[lowi];
-            vect = setRow(vect,lowi);
+            setRow(vect,lowi);
             demand[lowj] -= supply[lowi];
             supply[lowi] = 0;
         }
    
-        else if(supply[lowi] > demand[lowj]){
+        else if (supply[lowi] > demand[lowj]) {
             cost += vect[lowi][lowj] * demand[lowj];
             ibfs[lowi][lowj] = demand[lowj];
-            vect = setCol(vect,lowj);
+            setCol(vect,lowj);
             supply[lowi] -= demand[lowj];
             demand[lowj] = 0;
         }
    
-        else if(supply[lowi] == demand[lowj]){
+        // When supply[lowi] is equal to demand[lowj]
+        else {
             cost += vect[lowi][lowj] * demand[lowj];
             ibfs[lowi][lowj] = demand[lowj];
-            vect = setRow(vect,lowi);
-            vect = setCol(vect,lowj);
-            demand[lowj]=0;
-            supply[lowi]=0;
+            setRow(vect, lowi);
+            setCol(vect, lowj);
+            demand[lowj] = 0;
+            supply[lowi] = 0;
         }
     }
 
 
-    cout<<"Optimum Cost "<<cost<<endl;
-    cout<<"IBFS matrix:"<<endl;
+    std::cout << "Optimum Cost " << cost << std::endl;
+    std::cout << "IBFS matrix:" << std::endl;
 
-    for(int row=0;row<ibfs.size();row++){
-        for(int col=0;col<ibfs[row].size();col++){
-            cout<<ibfs[row][col]<<" ";
+    for (int row=0; row < ibfs.size(); row++) {
+        for (int col=0; col < ibfs[row].size(); col++) {
+            std::cout << ibfs[row][col] << " ";
         }
-        cout<<endl;
+        std::cout << std::endl;
     }
 
     return 0;
--- NWM.cpp
+++ NWM.cpp
@@ -1,125 +1,109 @@
 /*
 North West Corner Method:
-Considerations: The problem is balanced i.e supply = demand
-                There is no value of supply which is same as demand - coded but not tested
-Input: Source - no. of sources - int
-       Destination - no. of destinations - int
+Considerations: 1. The problem is balanced i.e supply = demand
+                2. There is no value of supply which is same as demand - coded but not tested
+Input: Source - no. of sources - int source
+       Destination - no. of destinations - int dest
        Matrix input of all the constraints coeffcients - 2D matrix mat
-       Supply - supply amount of each source - int array
-       Demand - demand of each destination - int array
-Output : Optimum cost - int
-         Initial basic feasible solution - 2D matrix IBFS
-Challenge : Make other entries 0 of same matrix mat instead of IBFS
+       Supply - supply amount of each source - int 1D array supply
+       Demand - demand of each destination - int 1D array demand
+Output : Optimum cost - int cost
+         Initial basic feasible solution - 2D matrix ibfs
 */
 
 #include <iostream>
-using namespace std;
-
-/*void input(){
-    int source,dest;
-
-    cout<<"Enter no. of sources and destination respectively:";
-    cin >> source >> dest;
-    int mat[source][dest];
-    cout<<"Entries of the matrix:";
-
-    //matrix input 
-    for(int i=0;i<source;i++){
-        for(int j=0;j<dest;j++){
-            cin>>mat[i][j];
-        }
-    }
-}*/
 
 int main(){
 
-int source,dest;
+    int source, dest;
 
-cout<<"Enter no. of sources and destination respectively:";
-cin >> source >> dest;
-int mat[source][dest];
-cout<<"Entries of the matrix:";
+    std::cout << "Enter no. of sources and destination respectively:";
+    std::cin >> source >> dest;
+    int mat[source][dest];
+    std::cout << "Entries of the matrix:";
 
-//matrix input 
-for(int i=0;i<source;i++){
-    for(int j=0;j<dest;j++){
-        cin>>mat[i][j];
-    }
-}
-
-int supply[source];
-int demand[dest];
-int IBFS[source][dest] = {}; 
-
-//supply and demand value in array
-cout<<"Enter supply quantity for each source";
-for (int i=0;i<source;i++){
-  cin>>supply[i];
-}
-
-cout<<"Enter demand quantity for each destination";
-for (int k=0;k<dest;k++){
-  cin>>demand[k];
-}
-
-//testing if supply==demand
-int supply_sum=0;
-int demand_sum=0;
-for(int i=0;i<dest;i++){
-   if(i<source){
-      supply_sum+=supply[i];}
-   demand_sum+=demand[i];
-}
-
-//needed variables
-int cost = 0;
-int sup=0;
-int dem=0;
-
-//main algo
-while(dem<dest && sup<source && supply_sum==demand_sum){
-    
-    if(demand[dem] < supply[sup]){
-        supply[sup]-=demand[dem];
-        cost += mat[sup][dem] * demand[dem];
-        //mat[sup][dem]=demand[dem];
-        IBFS[sup][dem] = demand[dem];
-        demand[dem]=0;
-        dem++;          
+    //matrix input 
+    for (int i=0; i < source; i++) {
+        for (int j=0; j < dest; j++) {
+            std::cin >> mat[i][j];
+        }
     }
 
-    else if(demand[dem]>supply[sup]){
-        demand[dem]-=supply[sup];
-        cost += mat[sup][dem] * supply[sup];
-        //mat[sup][dem]=supply[sup];
-        IBFS[sup][dem] = supply[sup];
-        supply[sup]=0;
-        sup++;
+    int supply[source];
+    int demand[dest];
+    int ibfs[source][dest] = {}; 
+
+    //supply and demand value in array
+    std::cout << "Enter supply quantity for each source";
+    for (int i=0; i < source; i++){
+        std::cin >> supply[i];
     }
 
-    else if(demand[dem]==supply[sup]){
-        cost += mat[sup][dem] * demand[dem];
-        //mat[sup][dem]=demand[dem];
-        IBFS[sup][dem] = demand[dem];
-        demand[dem]=0;
-        supply[sup]=0;
-        if(dem == dest-1 && sup == source-1)
-            break;
-        sup++;
-        dem++;
+    std::cout << "Enter demand quantity for each destination";
+    for (int k=0; k < dest; k++){
+        std::cin >> demand[k];
     }
-    
-}
 
-cout<<"Optimum cost "<<cost<<endl;
-
-cout<<"Displaying new matrix"<<endl;
-for(int i=0;i<source;i++){
-    for(int j=0;j<dest;j++){
-        cout<<IBFS[i][j]<<" ";
+    //testing if supply==demand
+    int supply_sum = 0;
+    int demand_sum = 0;
+    for (int i=0; i < dest; i++) {
+        if (i < source)
+            supply_sum+=supply[i];
+        demand_sum+=demand[i];
     }
-    cout<<endl;
-}
 
-return 0;
+    //needed variables
+    int cost = 0;
+    int sup = 0;
+    int dem = 0;
+
+    //main algo
+    while (dem < dest && sup < source && supply_sum == demand_sum) {
+        
+        if (demand[dem] < supply[sup]) {
+
+            supply[sup] -= demand[dem];
+            cost += mat[sup][dem] * demand[dem];
+            ibfs[sup][dem] = demand[dem];
+            demand[dem] = 0;
+            dem++;          
+
+        }
+
+        else if (demand[dem] > supply[sup]) {
+
+            demand[dem] -= supply[sup];
+            cost += mat[sup][dem] * supply[sup];
+            ibfs[sup][dem] = supply[sup];
+            supply[sup] = 0;
+            sup++;
+
+        }
+
+        else if (demand[dem] == supply[sup]) {
+
+            cost += mat[sup][dem] * demand[dem];
+            ibfs[sup][dem] = demand[dem];
+            demand[dem] = 0;
+            supply[sup] = 0;
+            if(dem == dest-1 && sup == source-1)
+                break;
+            sup++;
+            dem++;
+        }
+        
+    }
+
+    std::cout << "Optimum cost " << cost << std::endl;
+
+    std::cout << "Displaying new matrix" << std::endl;
+    for (int i=0; i < source; i++) {
+        for (int j=0; j < dest; j++) {
+            std::cout << ibfs[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return 0;
 }
